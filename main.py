0f3da5a7fd65dc4d389aaeffb82bb51fa75a1f5a
Alberto R. Ramos <rr.albertoroman@gmail.com>
2024-08-01 15:29:06 -0400

    Modified main program to use OOP

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-from threading import Thread
-from sensors import GPS,IMU
+from rov import ROV
+import RPi.GPIO as GPIO   
 
-gps = GPS("GT-U7", "/dev/ttyAMA0", 9600,0.5,False)
-imu = IMU(0x6B,0x19,0x1A,0x1B,0x38,0x68)
 
-print(gps,imu)
+GPIO.setmode(GPIO.BCM)
+config = {
+    'gps':{
+        'name': "GT-U7",
+        'port': "/dev/ttyAMA0",
+        'baudrate': 9600,
+        'timeout': 0.5,
+        'wait_for_init': False
+    },
+    'imu':{
+        'PWR_MGMT_1': 0x6B,
+        'SMPLRT_DIV': 0x19,
+        'CONFIG' : 0x1A,
+        'GYRO_CONFIG': 0x1B,
+        'INT_ENABLE': 0x38,
+        'DEV_ADDR': 0x68,
+        'bus': '1'
+    },
+    'motor1':{
+        'EN':24,
+        'IN1':22,
+        'IN2':23
+        },
+    'motor2':{
+        'EN':27,
+        'IN1':25,
+        'IN2':26
+        }
+}
 
-while True:
-    new_pos = gps.measure()
-    new_inertial_state = imu.measure()
+rov = ROV(config)
 
-    print(new_pos,new_inertial_state)
+rov.start()
+rov.control_motors(50,50)
+rov.control_motors(75,25)
+rov.control_motors(25,75)
